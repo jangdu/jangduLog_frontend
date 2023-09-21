308dcc8d28b0ed14f3ad0b1305056eb947ad1d8e
jangdu <jjd0324@gmail.com>
2023-09-22 02:14:54 +0900

    fix: currentTag로 관리하던 태그를 쿼리스트링으로 변경

--- src/components/post/PostList.js
+++ src/components/post/PostList.js
@@ -3,17 +3,22 @@ import LoadingUi from '../ui/LoadingUi';
 import axios from 'axios';
 import PostCard from './PostCard';
 import TagList from './tag/TagList';
+import { useSearchParams } from 'react-router-dom';
 
 export default function PostList() {
   const [posts, setPosts] = useState();
   const [currentPage, setCurrentPage] = useState(1);
-  const [currentTag, setCurrentTag] = useState(0);
   const [tags, setTags] = useState();
+
+  // eslint-disable-next-line no-unused-vars
+  const [searchParams, setSearchParams] = useSearchParams();
 
   useEffect(() => {
     const getAllPosts = async () => {
       const response = await axios.get(
-        `${process.env.REACT_APP_API_SERVERURL}/posts?page=${currentPage}&tagId=${currentTag}`,
+        `${
+          process.env.REACT_APP_API_SERVERURL
+        }/posts?page=${currentPage}&tagId=${searchParams.get('tagId') || 0}`,
         {
           withCredentials: true,
           headers: {
@@ -30,7 +35,7 @@ export default function PostList() {
     };
 
     getAllPosts();
-  }, [currentPage, currentTag]);
+  }, [currentPage, searchParams]);
 
   useEffect(() => {
     const getAllTags = async () => {
@@ -54,16 +59,16 @@ export default function PostList() {
     getAllTags();
   }, []);
 
+  // 태그가 변경될 때 페이지를 1로 초기화
+  useEffect(() => {
+    setCurrentPage(1);
+  }, [searchParams]);
+
   if (posts) {
     return (
       <div className="">
         <div className="max-w-md mx-auto my-3 sm:mb-8">
-          <TagList
-            tags={tags}
-            setCurrentTag={setCurrentTag}
-            currentTag={currentTag}
-            setCurrentPage={setCurrentPage}
-          />
+          <TagList tags={tags} currentTag={searchParams.get('tagId')} />
         </div>
         <div className="w-full max-w-2xl mx-auto flex flex-col gap-5">
           {posts.length > 0 &&
